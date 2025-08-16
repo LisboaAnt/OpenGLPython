0226f87d04c1883fbb80207d90c258daa9c99b3a
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2025-08-16 14:22:47 -0300

    Implementação de iluminação avançada na classe ShadedObjects, incluindo componentes ambientais, difusos e especulares. Adição da variável view_pos para calcular a direção da câmera e ajustes na normalização das normais. Atualização na inicialização do modelo teapot para incluir escala.

--- UdemyCurse/Section10/Engine2/ShadedObjects.py
+++ UdemyCurse/Section10/Engine2/ShadedObjects.py
@@ -19,11 +19,13 @@ out vec3 color;
 out vec3 normal;
 out vec3 fragpos;
 out vec3 light_pos;
+out vec3 view_pos;
 void main()
 {
-    light_pos = vec3( inverse(model_mat) * vec4(view_mat[3][0], view_mat[3][1], view_mat[3][2], 1));
+    light_pos = vec3(5, 5, 5);
+    view_pos = vec3( inverse(model_mat) * vec4(view_mat[3][0], view_mat[3][1], view_mat[3][2], 1));
     gl_Position = projection_mat * inverse(view_mat) * model_mat * vec4(position,1);
-    normal = vertex_normal;
+    normal = mat3(transpose(inverse(model_mat))) * vertex_normal;
     fragpos = vec3(model_mat * vec4(position, 1));
     color = vertex_color;
 }
@@ -35,15 +37,30 @@ in vec3 color;
 in vec3 normal;
 in vec3 fragpos;
 in vec3 light_pos;
+in vec3 view_pos;
 out vec4 frag_color;
 void main()
 {
     vec3 light_color = vec3(1, 0, 0);
+
+    //ambient
+    float a_strength = 0.2;
+    vec3 ambient = a_strength * light_color;
+
+    //diffuse
     vec3 norm = normalize(normal);
     vec3 light_dir = normalize(light_pos - fragpos);
     float diff = max(dot(norm, light_dir), 0.0);
     vec3 diffuse = diff * light_color;
-    frag_color = vec4(color * diffuse, 1);
+    
+    //specular
+    float s_strength = 0.8;
+    vec3 view_dir = normalize(view_pos - fragpos);
+    vec3 reflect_dir = normalize(-light_dir - norm);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
+    vec3 specular = s_strength * spec * light_color;
+
+    frag_color = vec4(color * (ambient + diffuse + specular), 1);
 }
 '''
 
@@ -76,7 +93,8 @@ class ShadedObjects(PyOGApp):
         self.teapot2 = LoadMesh("UdemyCurse/Section10/Engine2/models/teapot.obj", 
                                 self.program_id, 
                                 location=pygame.Vector3(-0.5, 0, 0),
-                                move_rotation=Rotation(1, pygame.Vector3(0, 1, 0)))
+                                move_rotation=Rotation(1, pygame.Vector3(0, 1, 0)),
+                                scale=pygame.Vector3(0.1, 0.1, 0.1))
         
         self.camera = Camera(self.program_id, self.screen_width, self.screen_height)
         glEnable(GL_DEPTH_TEST)
